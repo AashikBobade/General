b5288e922822a0c5598bbe5f765c5e396ce6aebf
AashikBobade <96589372+AashikBobade@users.noreply.github.com>
2022-01-23 20:21:02 +0530

    Update double_palindrome.py

--- double_palindrome.py
+++ double_palindrome.py
@@ -1,3 +1,8 @@
+'''A number is called a double palindrome if both the number and its square are palindromes. 
+For example, 11 is double palindrome as both 11 and 121 are palindromes. 
+Accept a positive integer n as input and print all the double palindromes less than or equal to n in ascending order.'''
+
+
 def is_pal(n):
     # n = str(n)
     # return n == n[::-1]
