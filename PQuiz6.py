3c6ecd01df87e68e27e38f584fcada46ba73d1a9
AashikBobade <96589372+AashikBobade@users.noreply.github.com>
2022-01-27 17:28:32 +0530

    Add files via upload

---
+++ PQuiz6.py
@@ -0,0 +1,18 @@
+def multiply(A, B, C):
+    temp =[]        
+    for i in range(len(A)):
+        temp.append([])
+        for j in range(len(B[0])):
+            temp[i].append(0)
+            for k in range(len(B)):
+                temp[i][j] += A[i][k] * B[k][j]
+    prod = []
+    for i in range(len(A)):
+        prod.append([])
+        for j in  range(len(C[0])):
+            prod[i].append(0)
+            for k in range(len(B[0])):
+                prod[i][j] += temp[i][k] * C[k][j]
+    return prod
+
+print(multiply([[1,2],[2,3]],[[1,2],[2,3]],[[1,2],[2,3]]))
